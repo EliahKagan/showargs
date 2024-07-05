a4ad4d7a732b82a3b1fbfbb56646052d08d5b45b
Eliah Kagan <degeneracypressure@gmail.com>
2024-07-05 17:31:38 -0400

    Substantially adjust style

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,19 @@
 //! This is not equivalent to using stdout or stderr. Even if all standard
 //! streams are redirected, this will still attempt to write to the console.
 
-use std::{env, fs, io::{Error, Write}};
+use std::{
+    env, fs,
+    io::{Error, Write},
+};
+
+const DEVICE_PATH: &str = if cfg!(windows) {
+    r#"\\.\CON"#
+} else {
+    "/dev/tty"
+};
 
 fn main() -> Result<(), Error> {
-    let mut console = fs::OpenOptions::new()
-        .write(true)
-        .open( if cfg!(windows) { r#"\\.\CON"# } else { "/dev/tty" })?;
+    let mut console = fs::OpenOptions::new().write(true).open(DEVICE_PATH)?;
 
     for (i, arg) in env::args().enumerate() {
         write!(console, "{i}: [{arg}]\n")?;
