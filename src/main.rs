3250d8af58c4a49214a512d21691e74067911761
Eliah Kagan <degeneracypressure@gmail.com>
2024-07-04 21:20:18 -0700

    Write to the Windows console device
    So that even if standard streams are redirected, the message
    appears.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
     for (i, arg) in std::env::args().enumerate() {
-        eprintln!("{i}: [{arg}]");
+        std::fs::write(r#"\\.\CON"#, format!("{i}: [{arg}]\n")).expect("can write to console");
     }
 }
