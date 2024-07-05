2a5594fbc5cabdee1739039a4b2a8684b1035974
Eliah Kagan <degeneracypressure@gmail.com>
2024-07-05 15:49:08 -0400

    Add a module documentation comment

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+//! Prints numbered command-line arguments to the Windows console device.
+//!
+//! This is not equivalent to using stdout or stderr. Even if all standard
+//! streams are redirected, this will still attempt to write to the console.
+//!
+//! Note that this implementation only works properly on Windows, because it
+//! uses a device path for the console. This path, `\\.\CON`, like its legacy
+//! short form `CON` and alternatives using `CONOUT$`, are specific to Windows.
+//!
+//! This could be made portable to Unix-like systems by using `/dev/tty`
+//! instead of `\\.\CON` when the platform is not Windows.
+
 use std::{env, fs, io::Write};
 
 fn main() {
