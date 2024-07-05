cbabd633476eec1b70d8add66b630fedaf8e0f95
Eliah Kagan <degeneracypressure@gmail.com>
2024-07-05 17:31:35 -0400

    Try and make portable
    This also makes it return a Result rather than panicking with
    two separate specific hard-coded messages, because (a) it's not
    obvious that the more long-windows panicking approach is better,
    and (b) the messages would not be quite right for both platforms.

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,18 @@
-//! Prints numbered command-line arguments to the Windows console device.
+//! Prints numbered command-line arguments to the console or terminal device.
 //!
 //! This is not equivalent to using stdout or stderr. Even if all standard
 //! streams are redirected, this will still attempt to write to the console.
-//!
-//! Note that this implementation only works properly on Windows, because it
-//! uses a device path for the console. This path, `\\.\CON`, like its legacy
-//! short form `CON` and alternatives using `CONOUT$`, is specific to Windows.
-//!
-//! This could be made portable to Unix-like systems by using `/dev/tty`
-//! instead of `\\.\CON` when the platform is not Windows.
 
-use std::{env, fs, io::Write};
+use std::{env, fs, io::{Error, Write}};
 
-fn main() {
+fn main() -> Result<(), Error> {
     let mut console = fs::OpenOptions::new()
         .write(true)
-        .open(r#"\\.\CON"#)
-        .expect("can open console device");
+        .open( if cfg!(windows) { r#"\\.\CON"# } else { "/dev/tty" })?;
 
     for (i, arg) in env::args().enumerate() {
-        write!(console, "{i}: [{arg}]\n").expect("can write to console");
+        write!(console, "{i}: [{arg}]\n")?;
     }
+
+    Ok(())
 }
